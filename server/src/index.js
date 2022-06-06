9c2efad730fbe5edf6805e60b13826f3f8a50e72
Benoît Hubert <benoithubert@gmail.com>
2022-06-06 09:45:34 +0200

    asyncify and install morgan

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const session = require('express-session');
+const morgan = require('morgan');
 const sliderCaptcha = require('@slider-captcha/core');
 
 const app = express();
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(
   session({
@@ -12,25 +14,25 @@ app.use(
   }),
 );
 
-app.get('/captcha/create', function (req, res) {
-  sliderCaptcha.create().then(function ({ data, solution }) {
-    req.session.captcha = solution;
-    req.session.save();
-    res.status(200).send(data);
-  });
+app.get('/captcha/create', async function (req, res) {
+  const { data, solution } = await sliderCaptcha.create();
+  console.log(solution, data);
+  req.session.captcha = solution;
+  req.session.save();
+  res.status(200).send(data);
 });
 
-app.post('/captcha/verify', function (req, res) {
+app.post('/captcha/verify', async function (req, res) {
   console.log(req.session);
-  sliderCaptcha
-    .verify(req.session.captcha, req.body)
-    .then(function (verification) {
-      if (verification.result === 'success') {
-        req.session.token = verification.token;
-        req.session.save();
-      }
-      res.status(200).send(verification);
-    });
+  const verification = await sliderCaptcha.verify(
+    req.session.captcha,
+    req.body,
+  );
+  if (verification.result === 'success') {
+    req.session.token = verification.token;
+    req.session.save();
+  }
+  res.status(200).send(verification);
 });
 
 const port = process.env.PORT || 6600;
